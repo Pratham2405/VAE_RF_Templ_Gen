c477630271ffa7565c4bbb3f9a5dab4203f9c428
Pratham Dhanoa <115894808+Pratham2405@users.noreply.github.com>
2026-01-15 15:06:35 +0530

    Update pdb_make.sh

--- scripts/pdb_make.sh
+++ scripts/pdb_make.sh
@@ -1,3 +1,3 @@
-# gmx trjconv -s 2erl_A_DataSource/2erl_A_prod_R2.tpr -f 2erl_A_DataSource/2erl_A_prod_R2_fit.xtc.xtc -o all_pdb_structures.pdb
+gmx trjconv -s 2erl_A_DataSource/2erl_A_prod_R2.tpr -f 2erl_A_DataSource/2erl_A_prod_R2_fit.xtc.xtc -o all_pdb_structures.pdb
 
 # When prompted, select the group you want (e.g., 0 for System, 1 for Protein)
