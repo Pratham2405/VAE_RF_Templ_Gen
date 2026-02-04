0072396492981940eb9a8c5a2a454b0332423fee
Pratham Dhanoa <115894808+Pratham2405@users.noreply.github.com>
2026-02-04 09:05:19 +0530

    Update dataset.py

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -5,11 +5,10 @@ from torch.utils.data import Dataset, DataLoader
 
 class C6DDataset(Dataset):
     def __init__(self, path):
-        data = torch.load(path, map_location="cpu")  # {'c6d': ..., 'mask': ..., 'params': ...}
+        data = torch.load(path, map_location="cpu") 
         c6d = data['c6d']   # [N, 40, 40, 4]
         mask = data['mask'] # [N, 40, 40]
 
-        # channels-last -> channels-first
         c6d = c6d.permute(0, 3, 1, 2).contiguous()  # [N, 4, 40, 40]
 
         self.x = c6d.float()
@@ -20,9 +19,3 @@ class C6DDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.x[idx], self.mask[idx]
-
-# Usage (in your training script):
-# data = torch.load(".../2erlA_c6d_with_mask.pt")
-# dataset = C6DDataset(data['c6d'], data['mask'])
-# from torch.utils.data import DataLoader
-# loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4, pin_memory=True)
